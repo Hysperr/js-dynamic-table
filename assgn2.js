1c9c7438dd17fcd2e9b4de9eda5a4344920410d3
Hysperr <Hysperr@users.noreply.github.com>
2017-09-17 03:28:52 -0400

    Added row preservation upon deletion
    The application now remembers the latest string entered to a widget
    after it is removed. If the widget is added back, the saved value will
    be displayed once again.

--- assgn2.js
+++ assgn2.js
@@ -1,29 +1,50 @@
 var num = 1;
+var addBackFlag = false;
+var addBackContent;
 
 function addRow() {
     num++;
     var row = document.getElementById('my-table').insertRow(-1);
     var cell0 = row.insertCell(0);
     var cell1 = row.insertCell(1);
-    cell0.innerHTML =
-        '                <form>\n' +
-        '                    <label for="tmp">\n' +
-        '                        ' + num + '\n' +
-        '                    </label>\n' +
-        '                        <input id="tmp" type="text" oninput="countInputLength(this)">\n' +
-        '                </form>';
-    cell1.innerHTML = cell0.getElementsByTagName('input')[0].value.length;
+    if (addBackFlag)
+    {
+        cell0.innerHTML =
+            '                <form>\n' +
+            '                    <label for="tmp">\n' +
+            '                        ' + num + '\n' +
+            '                    </label>\n' +
+            '                        <input id="tmp" type="text" value="' + addBackContent + '" oninput="countInputLength(this)">\n' +
+            '                </form>';
+        cell1.innerHTML = cell0.getElementsByTagName('input')[0].value.length;
+        addBackFlag = false;
+    }
+    else
+    {
+        cell0.innerHTML =
+            '                <form>\n' +
+            '                    <label for="tmp">\n' +
+            '                        ' + num + '\n' +
+            '                    </label>\n' +
+            '                        <input id="tmp" type="text" oninput="countInputLength(this)">\n' +
+            '                </form>';
+        cell1.innerHTML = cell0.getElementsByTagName('input')[0].value.length;
+    }
 }
 
 function removeRow() {
     var table = document.getElementById('my-table');
     if (table.rows.length > 2) {
+        var lastRow = table.rows[table.rows.length - 1];
         table.deleteRow(-1);
         num--;
     }
+    addBackContent = lastRow.getElementsByTagName('input')[0].value;
+    addBackFlag = true;
 }
 
 function countInputLength(obj) {
+    addBackFlag = false;    // start typing, add back is no more!
     var table = document.getElementById('my-table');
     var idx = obj.parentNode.parentNode.parentNode.rowIndex;
     table.rows[idx].cells[1].innerHTML = table.rows[idx].cells[0].getElementsByTagName('input')[0].value.length;
@@ -31,11 +52,11 @@ function countInputLength(obj) {
 
 function sortCells() {
     var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("my-table");
+    table = document.getElementById('my-table');
     switching = true;
     while (switching) {
         switching = false;
-        rows = table.getElementsByTagName("TR");
+        rows = table.getElementsByTagName('tr');
         for (i = 1; i < (rows.length - 1); i++) {
             shouldSwitch = false;
             x = rows[i].cells[0].getElementsByTagName('input')[0];
